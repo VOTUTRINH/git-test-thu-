864bd835a8df6ebcc26cc9a9a5b9ff4b02764df8
PHÚC VŨ HOÀNG <phuc9a134@gmail.com>
2022-09-30 15:08:16 +0700

    test ten

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-//Phuc
+//Phuc Vu
 public class MainActivity extends AppCompatActivity {
 
     @Override
